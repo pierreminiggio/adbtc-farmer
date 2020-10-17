a73c6c3e361f6427b6f75ef487763f9573320ca2
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-10-17 12:15:35 +0200

    promise

--- farmer.js
+++ farmer.js
@@ -1,22 +1,28 @@
 const puppeteer = require('puppeteer')
 
 /**
- * @param {boolean} debugMode 
+ * @param {boolean} debugMode
+ * 
+ * @returns {Promise}
  */
-function startFarming(debugMode = false) {
-    console.log('Launch !')
-    const browser = await puppeteer.launch({
-        headless: ! show,
-        args: [
-            '--window-size=800,500'
-        ]
-    })
-    
-    ! debugMode && args.push('--window-position=0,-600')
-    debugMode && console.log('Launched')
-    const page = await browser.newPage()
-    await page.goto('https://adbtc.top/index/enter')
+async function startFarming(debugMode = false) {
 
-    //browser.close()
+    return new Promise(async () => {
+        debugMode && console.log('Launch !')
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: [
+                '--window-size=800,500',
+                '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.271'
+            ]
+        })
+        
+        ! debugMode && args.push('--window-position=0,-600')
+        debugMode && console.log('Launched')
+        const page = await browser.newPage()
+        await page.goto('https://adbtc.top/index/enter')
+        //browser.close()
+    })
 }
 
+module.exports = startFarming
