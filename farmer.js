0a3fc5c685c2a82b82b3970143ba0808e465be08
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-10-17 12:58:15 +0200

    read ids

--- farmer.js
+++ farmer.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const puppeteer = require('puppeteer-extra')
 const stealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(stealthPlugin())
@@ -9,50 +10,65 @@ const solveReCaptcha = require('puppeteer-recaptcha-solver')
  * @returns {Promise}
  */
 async function startFarming(debugMode = false) {
+    return new Promise(() => {
+        fs.readFile('./ids.json', 'utf8', async (err, data) => {
+                if (err) {
+                    rejects()
+                }
 
-    return new Promise(async () => {
-        debugMode && console.log('Launch !')
-        const browser = await puppeteer.launch({
-            headless: false,
-            args: [
-                '--window-size=1280,720',
-                '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.271'
-            ]
-        })
-        
-        ! debugMode && args.push('--window-position=0,-600')
-        debugMode && console.log('Launched')
-        const page = await browser.newPage()
-        await page.goto('https://adbtc.top/index/enter')
+                /**
+                 * @typedef {Object} Ids 
+                 * @property {string} login 
+                 * @property {string} password
+                 */
 
-        debugMode && console.log('Selecting ReCaptcha...')
-        const captchaSelector = '.select-dropdown.dropdown-trigger'
-        await page.waitForSelector(captchaSelector)
-        await page.click(captchaSelector)
+                /** @type {Ids} ids */
+                const ids = JSON.parse(data)
 
-        const reCaptchaSelector = 'li[tabindex="0"]:nth-of-type(2)'
-        await page.waitForSelector(reCaptchaSelector)
-        await page.click(reCaptchaSelector)
+                debugMode && console.log('Launch !')
+                const browser = await puppeteer.launch({
+                    headless: false,
+                    args: [
+                        '--window-size=1280,720',
+                        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.271'
+                    ]
+                })
+                
+                ! debugMode && args.push('--window-position=0,-600')
+                debugMode && console.log('Launched')
+                const page = await browser.newPage()
+                await page.goto('https://adbtc.top/index/enter')
 
-        debugMode && console.log('Selected ! Entering username and password...')
-        const usernameInputSelector = '#pochka'
-        await page.waitForSelector(usernameInputSelector)
-        const passwordInputSelector = '#paszwort'
-        await page.waitForSelector(passwordInputSelector)
-        await page.evaluate((usernameInputSelector, passwordInputSelector) => {
-            document.querySelector(usernameInputSelector).value = 'test'
-            document.querySelector(passwordInputSelector).value = 'test'
-        }, usernameInputSelector, passwordInputSelector)
+                debugMode && console.log('Selecting ReCaptcha...')
+                const captchaSelector = '.select-dropdown.dropdown-trigger'
+                await page.waitForSelector(captchaSelector)
+                await page.click(captchaSelector)
 
-        debugMode && console.log('Entered ! Waiting for ReCaptcha...')
-        const reCaptchaAnchorLabel = '#recaptcha-anchor-label'
-        await page.waitForSelector(reCaptchaAnchorLabel)
+                const reCaptchaSelector = 'li[tabindex="0"]:nth-of-type(2)'
+                await page.waitForSelector(reCaptchaSelector)
+                await page.click(reCaptchaSelector)
 
-        debugMode && console.log('Waited ! Solving ReCaptcha...')
-        await solveReCaptcha(page)
-        debugMode && console.log('Solved ! Loging in...')
+                debugMode && console.log('Selected ! Entering username and password...')
+                const usernameInputSelector = '#pochka'
+                await page.waitForSelector(usernameInputSelector)
+                const passwordInputSelector = '#paszwort'
+                await page.waitForSelector(passwordInputSelector)
+                await page.evaluate((usernameInputSelector, passwordInputSelector, login, password) => {
+                    document.querySelector(usernameInputSelector).value = login
+                    document.querySelector(passwordInputSelector).value = password
+                }, usernameInputSelector, passwordInputSelector, ids.login, ids.password)
 
-        //browser.close()
+                debugMode && console.log('Entered ! Waiting for ReCaptcha...')
+                const reCaptchaAnchorLabel = '#recaptcha-anchor-label'
+                await page.waitForSelector(reCaptchaAnchorLabel)
+
+                debugMode && console.log('Waited ! Solving ReCaptcha...')
+                await solveReCaptcha(page)
+                debugMode && console.log('Solved ! Loging in...')
+
+                //browser.close()
+            }
+        )
     })
 }
 
