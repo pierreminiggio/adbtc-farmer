9d347581f1e0cda9e114b1cb261ac87f2722762c
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-10-18 10:57:25 +0200

    move args to constant

--- farmer.js
+++ farmer.js
@@ -32,16 +32,17 @@ async function startFarming(debugMode = false) {
     return new Promise(async (resolve) => {
         debugMode && console.log('Launch !')
         /** @type {import('puppeteer').Browser} browser */
+        const args = [
+            '--window-size=1280,720',
+            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.271'
+        ]
+        ! debugMode && args.push('--window-position=0,-800')
         const browser = await puppeteer.launch({
             headless: false,
-            args: [
-                '--window-size=1280,720',
-                '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.271'
-            ]
+            args: args
         })
         
         try {
-            ! debugMode && args.push('--window-position=0,-800')
             debugMode && console.log('Launched')
             const page = await browser.newPage()
             await page.goto('https://adbtc.top/index/enter')
